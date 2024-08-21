9582c3ba79bcf5cf046e1d3cd3380a8c5b760186
Nadeŭka <nadevko@riseup.net>
2024-08-22 01:17:29 +0300

    Use nix shell

--- shell.nix
+++ shell.nix
@@ -1,5 +1,21 @@
-with import <nixpkgs> { };
-mkShell {
-  nativeBuildInputs = [ clang-tools ];
-  NIX_LD = "${stdenv.cc.libc_bin}/bin/ld.so";
+{
+  pkgs ? import <nixpkgs> { overlays = [ (import <bsuir-tex/nixpkgs>) ]; },
+}:
+with pkgs;
+mkShell rec {
+  name = "AnDS-2";
+
+  vscode-settings = writeText "settings.json" (
+    builtins.toJSON { "clangd.path" = "${pkgs.clang-tools}/bin/clangd"; }
+  );
+
+  packages = [
+    zig
+    zls
+  ];
+
+  shellHook = ''
+    mkdir .vscode &>/dev/null
+    cp ${vscode-settings} .vscode/settings.json
+  '';
 }
